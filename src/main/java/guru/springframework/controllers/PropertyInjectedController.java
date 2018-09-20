c0066e87a10bcbdec9eb3f071f2b4b60dd96fcf5
developerdave <dave.green@moneysupermarket.com>
2018-09-20 14:39:45 +0100

    Qualifier annotation

--- src/main/java/guru/springframework/controllers/PropertyInjectedController.java
+++ src/main/java/guru/springframework/controllers/PropertyInjectedController.java
@@ -2,12 +2,15 @@ package guru.springframework.controllers;
 
 import guru.springframework.services.GreetingService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class PropertyInjectedController {
 
-	@Autowired public GreetingService greetingService;
+	@Autowired
+	@Qualifier("greetingServiceImpl")
+	public GreetingService greetingService;
 
 	public String sayHello() {
 		return greetingService.sayGreeting();
