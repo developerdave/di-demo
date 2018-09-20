56ea23d1cfea448bbfd8574a96de5be864ec874a
developerdave <dave.green@moneysupermarket.com>
2018-09-20 14:18:46 +0100

    Spring injection

--- src/main/java/guru/springframework/controllers/PropertyInjectedController.java
+++ src/main/java/guru/springframework/controllers/PropertyInjectedController.java
@@ -1,16 +1,15 @@
 package guru.springframework.controllers;
 
 import guru.springframework.services.GreetingService;
-import guru.springframework.services.GreetingServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 
-/**
- * Everything not to do.
- */
+@Controller
 public class PropertyInjectedController {
 
-	public GreetingService greetingService;
+	@Autowired public GreetingService greetingService;
 
-	String sayHello() {
+	public String sayHello() {
 		return greetingService.sayGreeting();
 	}
 }
