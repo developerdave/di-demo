2ab6daff53ddb9d2cd8fe47919c99a56821950a5
developerdave <dave.green@moneysupermarket.com>
2018-10-15 11:32:29 +0100

    Use of application.properties

--- src/main/java/guru/springframework/config/PropertyConfig.java
+++ src/main/java/guru/springframework/config/PropertyConfig.java
@@ -12,15 +12,7 @@ import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.core.env.Environment;
 
 @Configuration
-//@PropertySource({"classpath:datasource.properties", "classpath:jms.properties"})
-@PropertySources({
-        @PropertySource("classpath:datasource.properties"),
-        @PropertySource("classpath:jms.properties")
-})
 public class PropertyConfig {
-
-  @Autowired
-  Environment env;
 
   @Value("${guru.username}")
   String user;
@@ -43,7 +35,7 @@ public class PropertyConfig {
   @Bean
   public FakeDataSource fakeDataSource() {
     FakeDataSource fakeDataSource = new FakeDataSource();
-    fakeDataSource.setUser(env.getProperty("USERNAME"));
+    fakeDataSource.setUser(user);
     fakeDataSource.setPassword(password);
     fakeDataSource.setUrl(url);
 
